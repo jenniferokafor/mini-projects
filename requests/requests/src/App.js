ee9b19ff3fd93fb6a9cadaaa194e839cc9296f98
Jennifer Okafor <jennakafor00@gmail.com>
2022-07-25 06:25:56 +0100

    adding request project

---
+++ requests/requests/src/App.js
@@ -0,0 +1,13 @@
+import FirstRequest from "./components/requests/first-request"
+import Title from "./components/Title"
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <FirstRequest />
+    </div>
+  )
+}
+
+export default App
